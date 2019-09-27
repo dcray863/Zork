669cc16551e670756361b4351ea1a4c8b7f397b8
TheDavster <over8000@hotmail.com>
2019-09-27 03:54:00 -0400

    10 - File Processing

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
@@ -25,9 +26,15 @@ namespace Zork
                 return Rooms[Location.Row, Location.Column];
             }
         }
+
+        private static readonly Dictionary<string, Room> RoomMap;
+
         static void Main(string[] args)
         {
-            InitializeRoomDescriptions();
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
+            InitializeRoomDescriptions(roomsFilename);
+
             Console.WriteLine("Welcome to Zork!");
 
             Room previousRoom = null;
@@ -70,6 +77,16 @@ namespace Zork
             }
         }
 
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                RoomMap[room.Name] = room;
+            }
+
+        }
+
         private static bool Move(Commands command)
         {
             Assert.IsTrue(IsDirection(command), "Invalid Direction");
@@ -100,25 +117,36 @@ namespace Zork
             return isValidMove;
         }
 
-        private static void InitializeRoomDescriptions()
+        private static void InitializeRoomDescriptions(string roomsFilename)
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach(Room room in Rooms)
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach(string line in lines)
             {
-                roomMap[room.Name] = room;
+                string[] fields = line.Split(fieldDelimiter);
+                if(fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid record.");
+                }
+
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
+
+                RoomMap[name].Description = description;
             }
+        }
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
 
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
         }
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
