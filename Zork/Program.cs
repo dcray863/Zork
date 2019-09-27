457804127dafbd739e462673d6ab48337b523330
TheDavster <over8000@hotmail.com>
2019-09-27 01:49:07 -0400

    5 - The Game World

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
     class Program
     {
+        private static int locIndex = 1;
+        private static readonly string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.WEST,
+            Commands.EAST
+        };
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -11,6 +21,7 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
+                Console.WriteLine(Rooms[locIndex]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
@@ -30,7 +41,12 @@ namespace Zork
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = $"You moved {command}.";
+                        if(Move(command) == false)
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        else
+                            outputString = $"You moved {command}.";
                         break;
 
                     default:
@@ -42,6 +58,29 @@ namespace Zork
             }
         }
 
+        private static bool Move(Commands command)
+        {
+            Assert.IsTrue(IsDirection(command), "Invalid Direction");
+
+            bool moveOK = false;
+            switch (command)
+            {
+                case Commands.WEST when locIndex > 0:
+                    locIndex--;
+                    moveOK = true;
+                    break;
+                case Commands.EAST when locIndex < Rooms.Length - 1:
+                    locIndex++;
+                    moveOK = true;
+                    break;
+
+                default:
+                    break;
+            }
+            return moveOK;
+        }
+
+        private static bool IsDirection(Commands command) => Directions.Contains(command);
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
