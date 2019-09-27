f6221214dba82c09e3f5174996c011248dba0d98
TheDavster <over8000@hotmail.com>
2019-09-27 02:39:52 -0400

    8 - Object Oriented Rooms

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,10 +6,10 @@ namespace Zork
     class Program
     {
         private static (int Row, int Column) Location = (1, 1);
-        private static readonly string[,] Rooms = {
-            { "Rocky Trail", "South of House", "Canyon View"},
-            { "Forest", "West of House", "Behind House" },
-            { "Dense Woods", "North of House", "Clearing" }
+        private static readonly Room[,] Rooms = {
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
         private static readonly List<Commands> Directions = new List<Commands>
         {
@@ -18,7 +18,7 @@ namespace Zork
             Commands.WEST,
             Commands.EAST
         };
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -27,6 +27,7 @@ namespace Zork
         }
         static void Main(string[] args)
         {
+            InitializeRoomDescriptions();
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
@@ -36,36 +37,30 @@ namespace Zork
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. \n" +
-                            "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        if(Move(command) == false)
+                        if(!Move(command))
                         {
-                            outputString = "The way is shut!";
+                            Console.WriteLine("The way is shut!");
                         }
-                        else
-                            outputString = $"You moved {command}.";
                         break;
 
                     default:
-                        outputString = "Unknown command.";
+                        Console.WriteLine("Unknown command.");
                         break;
                 }
-
-                Console.WriteLine(outputString);
             }
         }
 
@@ -97,6 +92,21 @@ namespace Zork
                     break;
             }
             return isValidMove;
+        }
+
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
         }
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
