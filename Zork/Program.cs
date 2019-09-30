75ba5bb80c707d2366d6fe316702932650a6045a
dcray863 <54968281+dcray863@users.noreply.github.com>
2019-09-30 14:16:22 -0400

    12 - A Larger World - Resubmit
    Had trouble merging program.cs. Ended up just  rewriting it.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,71 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
 
 namespace Zork
 {
     class Program
     {
-        private static readonly string[] Rooms =
-        {
-            "Forest", "West of House", "Behind House", "Clearing", "Canyon View"
-        };
-        private static int locationIndex = 1;
-
         static void Main(string[] args)
         {
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+            Game game = Game.Load(gameFilename);
             Console.WriteLine("Welcome to Zork!");
-
-            Commands command = Commands.UNKNOWN;
-            while(command != Commands.QUIT)
-            {
-                Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                string outputString;
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
-                        break;
-                    case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. " +
-                            "\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
-                        break;
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (Move(command))
-                        {
-                            outputString = $"You moved {command}.\n" + Rooms[locationIndex];
-                        }
-                        break;
-
-                    default:
-                        outputString = "Unknown command.";
-                        break;
-                }
-
-                Console.WriteLine(outputString);
-            }
+            game.Run();
+            Console.WriteLine("Thank you for playing");
         }
 
-        private static bool Move(Commands command)
+        private enum CommandLineArguments
         {
-            bool canMove = false;
-
-            switch (command)
-            {
-
-
-            }
-            return canMove;
+            GameFilename = 0
         }
-
-        private static Commands ToCommand(string commandString) =>
-            Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
     }
 }
